26ec7bc9f04518f7ef8e93b0a387ef480a838982
nem0 <rrivera@haverford.edu>
2020-05-31 01:18:53 -0400

    added AES implementation that initalizes its counter the same each time

---
+++ faulty_aes_ctr.py
@@ -0,0 +1,69 @@
+#Thank you to Levente Buttyan (CrySyS Labs) for the design
+
+import sys, getopt
+import Crypto.Cipher.AES
+import Crypto.Util.Counter
+
+keystring = ''
+inputfile = ''
+outputfile = ''
+
+try:
+   opts, args = getopt.getopt(sys.argv[1:],"hk:i:o:")
+except getopt.GetoptError:
+   print('aes_ctr.py -k <keystring> -i <inputfile> -o <outputfile>')
+   sys.exit(2)
+for opt, arg in opts:
+   if opt == '-h':
+      print('aes_ctr.py -k <keystring> -i <inputfile> -o <outputfile>')
+      sys.exit()
+   elif opt == "-k":
+      keystring = arg
+   elif opt == "-i":
+      inputfile = arg
+   elif opt == "-o":
+      outputfile = arg
+#print 'Key string is "', keystring
+#print 'Input file is "', inputfile
+#print 'Output file is "', outputfile
+
+if len(keystring) != 16:
+   print('Error: key string must be 16 character long')
+   sys.exit(2)
+
+if len(inputfile) == 0:
+   print('Error: name of input file is missing')
+   sys.exit(2)
+
+if len(outputfile) == 0:
+   print('Error: name of output file is missing')
+   sys.exit(2)
+
+ifile = open(inputfile, "rb")
+ofile = open(outputfile, "wb")
+
+ctr = Crypto.Util.Counter.new(128)
+cipher = Crypto.Cipher.AES.new(keystring.encode("ASCII"), Crypto.Cipher.AES.MODE_CTR, counter=ctr)
+
+eof = False
+while (eof == False):
+    plainblock = ifile.read(16)
+    if len(plainblock) == 16:
+        #print plainblock.encode("hex")
+        cipherblock = cipher.encrypt(plainblock)
+        ofile.write(cipherblock)
+    else:
+        eof = True       
+        if len(plainblock) > 0:
+            # padding to fit block size
+            plainblock += b'\x00'
+            for i in range(16-len(plainblock)):
+                plainblock += b'\x01'
+            # print plainblock.encode("hex")
+            cipherblock = cipher.encrypt(plainblock) 
+            ofile.write(cipherblock)
+ifile.close() 
+ofile.close()
+
+print('Done.')
+
