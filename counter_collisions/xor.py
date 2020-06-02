12e20aaca279c7c43fb08f281efcbb153c756728
nem0 <rrivera@haverford.edu>
2020-06-01 22:01:21 -0400

    adding padding oracle attack files

---
+++ counter_collisions/xor.py
@@ -0,0 +1,21 @@
+# Russell Rivera
+# XOR utilities
+
+def xor_strings(a, b, encoding):
+	""" a XOR b with format "encoding", e.g., "utf-8" """
+	return a.encode(encoding) ^ b.encode(encoding)
+
+	
+
+	
+
+
+def read_data(filename):
+	with open(filename, "r") as data:
+		
+
+
+
+if __name__ == "__main__":
+
+			
